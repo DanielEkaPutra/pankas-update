6808a67a78afe0faaec64c2a106b48e43b7aed97
ezrasenaputra <ezrasenaputra@gmail.com>
2021-08-30 22:17:08 +0700

    Update Form Meeting 30 Agustus

--- application/views/tambah-data/input-data-jemaat.php
+++ application/views/tambah-data/input-data-jemaat.php
@@ -8,16 +8,63 @@
                         <h5>Input Data Jemaat</h5>
                     </div>
                     <div class="card-block">
+                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <h4 class="sub-title">Sektor & Rayon</h4>
+                                                <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Sektor</label>
+                                                    <div class="col-sm-10">
+                                                    <select name="select" class="form-control">
+                                                                <option value="1">Yerusalem</option>
+                                                                <option value="2">Bethel</option>
+                                                                <option value="3">Sion</option>
+                                                                <option value="4">Hermon</option>
+                                                                <option value="5">Yerikho</option>
+                                                                <option value="6">Karmel</option>
+                                                                <option value="7">Pniel</option>
+                                                                <option value="8">Nazaret</option>
+                                                                <option value="9">Moria</option>
+                                                                <option value="10">Getsemani</option>
+                                                                <option value="11">Betesda</option>
+                                                                <option value="12">Betlehem</option>
+                                                                <option value="13">Ebenhaezer </option>
+                                                </select>
+                                                </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">No Rayon</label>
+                                                <div class="col-sm-10">
+                                                    <select name="select" class="form-control">
+                                                                <option value="1">1</option>
+                                                                <option value="2">2</option>
+                                                                <option value="3">3</option>
+                                                                <option value="4">4</option>
+                                                                <option value="5">5</option>
+                                                                <option value="6">6</option>
+                                                                <option value="7">7</option>
+                                                                <option value="8">8</option>
+                                                                <option value="9">9</option>
+                                                                <option value="10">10</option>
+                                                                <option value="11">11</option>
+                                                                <option value="12">12</option>
+                                                                <option value="13">13</option>
+                                                                <option value="14">14</option>
+                                                                <option value="15">15</option>
+                                                </select>
+                                                </div>
+                                            </div>
+                                            </div>
+                                        </div>
                         <h4 class="sub-title">Jemaat</h4>
                         <form>
                             <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">ID Jemaat</label>
+                                <label class="col-sm-2 col-form-label">ID KWJ</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control" placeholder="10101 (5 angka)">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">No Keluarga</label>
+                                <label class="col-sm-2 col-form-label">ID Jemaat</label>
                                 <div class="col-sm-10">
                                     <input type="text" class="form-control"
                                     placeholder="1">
@@ -67,7 +114,11 @@
                                     <div class="col-sm-10">
                                     <select name="select" class="form-control">
                                             <option value="1">Kepala Keluarga</option>
-                                            <option value="0">Anggota Keluarga</option>
+                                            <option value="0">Istri</option>
+                                            <option value="0">Anak</option>
+                                            <option value="0">Kakek</option>
+                                            <option value="0">Nenek</option>
+                                            <option value="0">Sodara</option>
                                         </select>
                                     </div>
                                 </div>
@@ -197,23 +248,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <h4 class="sub-title">Sektor & Rayon</h4>
-                                            <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Nama Sektor</label>
-                                                <div class="col-sm-10">
-                                                <input type="text" class="form-control">
-                                            </div>
-                                        </div>
-                                            <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">No Rayon</label>
-                                                <div class="col-sm-10">
-                                                <input type="text" class="form-control">
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </div>
+                                   
 
                                     <div class="row d-flex justify-content-end">
                                     <button class="btn btn-primary waves-effect waves-light mb-5 mr-5" type="submit">Submit</button>
